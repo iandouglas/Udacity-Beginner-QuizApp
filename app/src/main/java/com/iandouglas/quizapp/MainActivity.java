22c6ca43f41818168b6fc1d1a7031b401d14890c
ian douglas <ian.douglas@iandouglas.com>
2016-10-30 02:04:18 -0600

    changes based on feedback

--- app/src/main/java/com/iandouglas/quizapp/MainActivity.java
+++ app/src/main/java/com/iandouglas/quizapp/MainActivity.java
@@ -25,9 +25,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void scoreHandler(View view) {
-        int score = calculateScore();
-        CharSequence answer = String.valueOf(score) + getString(R.string.score_end);
-        Toast.makeText(view.getContext(), answer, Toast.LENGTH_LONG).show();
+        try {
+            String answer = getString(R.string.perfect_score);
+            int score = calculateScore();
+            if (score < 4) {
+                answer = String.valueOf(score) + getString(R.string.score_end);
+            }
+            Toast.makeText(view.getContext(), answer, Toast.LENGTH_LONG).show();
+        } catch (NullPointerException e) {
+            Toast.makeText(view.getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
     private int calculateScore() throws NullPointerException {
@@ -37,27 +44,39 @@ public class MainActivity extends AppCompatActivity {
         RadioGroup rg = (RadioGroup) findViewById(R.id.q1_radiogroup);
         if (rg.getCheckedRadioButtonId() == R.id.rbtn_question1_2) {
             score += 1;
+        } else if (rg.getCheckedRadioButtonId() == -1) {
+            throw new NullPointerException(getString(R.string.q1_noanswer));
         }
 
         // question 2
         Spinner spinner = (Spinner) findViewById(R.id.q2_answers_spinner);
         int position = spinner.getSelectedItemPosition();
-        if (position == 2) {
+        if (position == 3) {
             score += 1;
+        } else if (position == 0) {
+            throw new NullPointerException(getString(R.string.q2_noanswer));
         }
 
         // question 3
         CheckBox cb1 = (CheckBox) findViewById(R.id.q3_checkbox1);
+        CheckBox cb2 = (CheckBox) findViewById(R.id.q3_checkbox2);
         CheckBox cb3 = (CheckBox) findViewById(R.id.q3_checkbox3);
-        if (cb1.isChecked() && cb3.isChecked()) {
+        CheckBox cb4 = (CheckBox) findViewById(R.id.q3_checkbox4);
+        if (cb1.isChecked() && !cb2.isChecked() && cb3.isChecked() && !cb4.isChecked()) {
             ++score;  //so we adding 1 to the score
+        } else if (!cb1.isChecked() && !cb2.isChecked() && !cb3.isChecked() && !cb4.isChecked()) {
+            throw new NullPointerException(getString(R.string.q3_noanswer));
         }
 
         // question 4
         EditText setvar = (EditText) findViewById(R.id.q4_answer);
         String answerText = setvar.getText().toString();
-        if (answerText.toLowerCase().replaceAll("\\s", "").equals("a=5")) {
-            ++score;
+        if (answerText.length() != 0) {
+            if (answerText.toLowerCase().replaceAll("\\s", "").equals(getString(R.string.q4_rightanswer))) {
+                ++score;
+            }
+        } else {
+            throw new NullPointerException(getString(R.string.q4_noanswer));
         }
 
         return score;
